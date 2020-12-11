29036d0ec76fa4f801e20ae399a5bba6687bdc54
Mac <mac@Macs-MacBook-Pro-2.local>
2020-12-11 14:53:36 +0700

    adds to resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 7.30.0 hello world
+                    Laravel 7.30.0 hello hi
                 </div>
 
                 <div class="links">
