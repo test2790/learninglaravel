1953dce7b6cf80d73112f69b4c6438b1e66b1ac4
test2790 <testting279@gmail.com>
2021-12-10 08:38:17 +0700

    6 commit

--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Login</title>
+<title>Login Form</title>
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
